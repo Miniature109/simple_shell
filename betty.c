c5adec78099d8b13a7cfd87df81e89ae42dfa3c0
Miniature109 <tumininade@gmail.com>
2023-11-13 17:52:07 -0800

    betty

---
+++ betty.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_betty - Prints "Hello, betty!" to the console.
+ */
+void print_betty(void)
+{
+    printf("Hello, betty!\n");
+}
+
+/**
+ * main - The entry point of the program.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+    print_betty();
+    return (0);
+}
